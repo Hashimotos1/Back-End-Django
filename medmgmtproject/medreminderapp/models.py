aedd9ae4f1ce0ce86f93eb49a328684eb8df209c
Hashimotos1 <66645069+Hashimotos1@users.noreply.github.com>
2021-05-03 19:41:06 -0400

    Update models.py

--- medmgmtproject/medreminderapp/models.py
+++ medmgmtproject/medreminderapp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+#create User class model to map to the User database table
+class User(models.Model):
+	username = models.CharField(max_length=50, primary_key =True)
+	password = models.CharField(max_length=50)
+	name = models.CharField(max_length=50)
+	email = models.CharField(max_length=50)
+	phone_number = models.CharField(max_length=15)
+	age = models.IntegerField()
+	height = models.IntegerField()
+	weight = models.FloatField()
+
+#create Medication class model to map to the Medication database table
+class Medication(models.Model)
+	med_name = models.CharField(max_length = 25, primary_key=True)
+	med_dose_num = models.FloatField()
+	med_dose_unit = models.CharField(max_length = 15)
+	med_freq_num = models.FloatField()
+	med_freq_unit = models.CharField(max_length =15)
+	med_route = models.CharField(max_length = 25)
+	med_timing = models.TimeField()
+
+#Create Reminder class model to map to the Reminder database table
+class Reminder(models.Model)
+	med_name = models.ForeignKey(Medication)
+	med_dose_num = models.ForeignKey(Medication)
+	med_dose_unit = models.ForeignKey(Medication)
+	med_route = models.ForeignKey(Medication)
+	reminder_time = models.DataTimeField()
+	reminder_note = models.CharField(max_length=255, primary_key =True)
