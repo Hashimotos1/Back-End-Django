601abe062ae80373be77026a1e1526edb09ab8b4
Hashimotos1 <66645069+Hashimotos1@users.noreply.github.com>
2021-05-04 22:55:01 -0400

    Update SignUpView

--- medmgmtproject/medreminderapp/urls.py
+++ medmgmtproject/medreminderapp/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 
 from .views import home_handler, medications_handler, reminders_handler, history_handler, login_handler
 
+from .views import SignUpView
+
 urlpatterns = [
     path('', home_handler, name='Home'),
     path('medications', medications_handler, name='Medications'),
     path('reminders', reminders_handler, name='Reminders'),
     path('history', history_handler, name='History'),
-    path('login', login_handler, name='Login')
+    path('login', login_handler, name='Login'),
+    path('signup/', SignUpView.as_view(), name='Signup')
 ]
